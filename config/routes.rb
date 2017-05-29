bb853bed84352d5afbcbcf7e7957e4d1b2a665e2
Fatos Morina <fatosi.morina@gmail.com>
2017-05-30 00:04:31 +0200

    Rearrange routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  root 'welcome#index'
-
   devise_for :users
+
+  resources :documents
 
   authenticated :user do
     root "documents#index", as: "authenticated_root"
   end
 
-  resources :documents
-
+   root 'welcome#index'
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
