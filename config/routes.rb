36b8015e17b445d54dd10c50897850a71b7bf267
Fatos Morina <fatosi.morina@gmail.com>
2017-05-29 15:58:34 +0200

    Generate User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  root 'welcome#index' 
+  root 'welcome#index'
+
+  devise_for :users
 
   resources :documents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
